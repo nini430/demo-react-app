4cbec717b45985ff466d1d97d035a668b06fdaae
devopschannel23 <devopschannel23@gmail.com>
2025-12-14 21:54:22 +0530

    my first commit for node js app

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>React Demo App Built for Github Action Testing</h1>
+      <p>Github Action Testing</p>
+    </div>
+  );
+}
+
+export default App;
