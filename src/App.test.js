a8281d6be2f7f673a32f600455a7938855802fe2
devopschannel23 <devopschannel23@gmail.com>
2025-12-14 22:21:13 +0530

    fixed worflow file

--- src/App.test.js
+++ src/App.test.js
@@ -3,5 +3,5 @@ import App from "./App";
 
 test("renders heading", () => {
   render(<App />);
-  expect(screen.getByText(/React Demo App/i)).Not.toBeInTheDocument();
+  expect(screen.getByText(/React Demo App/i)).toBeInTheDocument();
 });
