3b97a3df58e57b43e3e4c3bb84ccecdf6d09c466
nini430 <105129369+nini430@users.noreply.github.com>
2026-05-02 10:01:44 +0400

    remove h1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 function App() {
   return (
     <div style={{ padding: 40 }}>
-      <h1>Welcome to Devops Channel!</h1>
       <p>React Demo App Built for Github Action Testing</p>
     </div>
   );
